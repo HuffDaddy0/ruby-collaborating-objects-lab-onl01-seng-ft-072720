3533278668af0a008f094e5a9ca8b16158a6beb0
Tyler Huffman <ty.huff24@gmail.com>
2020-08-11 19:55:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,3 +15,7 @@ def new_by_filename(filename)
   names = filename.split(" - ")
   song = Song.new(names[0])
   song.artist = names[2]
+end
+
+def artist_name=(name)
+  self.artist.name = find_or_create_by_name(name)
