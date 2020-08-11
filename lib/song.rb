74e792f55b27624d15cc0c8ccd3168bbdcf1570e
Tyler Huffman <ty.huff24@gmail.com>
2020-08-11 19:52:26 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+class Song
+  attr_accessor :title, :artist
+
+@@all = []
+  def initialize(title)
+    self.title = title
+    @@all << self
+  end
+
+def self.all
+  @@all
+end
+
+def new_by_filename(filename)
+  names = filename.split(" - ")
+  song = Song.new(names[0])
+  song.artist = names[2]
+  
